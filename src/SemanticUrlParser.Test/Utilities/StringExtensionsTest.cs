8aba84de18fcd02aa5291345df02ae339f2c2046
Andreas Grünwald <andreas@grynwald.net>
2020-09-05 12:03:11 +0200

    WIP: Add GitHub remote url parser
    Add tests for StringExtensions

---
+++ src/SemanticUrlParser.Test/Utilities/StringExtensionsTest.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace Grynwald.SemanticUrlParser.Test.Utilities
+{
+    /// <summary>
+    /// Tests for <see cref="StringExtensions"/>
+    /// </summary>
+    public class StringExtensionsTest
+    {
+        [Theory]
+        [InlineData("some-string", "", "some-string")]
+        [InlineData("some-string", "-string", "some")]
+        [InlineData("some-string", "suffix", "some-string")]
+        public void RemoteSuffix_returns_expected_string(string input, string suffix, string expectedResult)
+        {
+            // ARRANGE
+
+            // ACT 
+            var actualResult = input.RemoveSuffix(suffix);
+
+            // ASSERT
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
